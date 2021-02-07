3b412bf444954fb2dc91fbe48549b49a7501345a
kyassini <kevin_yassini@student.uml.edu>
2021-02-07 00:04:02 -0500

    Update manipulation class

--- src/functions/manipulation_class.cpp
+++ src/functions/manipulation_class.cpp
@@ -1,12 +1,23 @@
+/*****************************************************************
+  Manipulation Function Definitions
+*****************************************************************/
+
 #include "manipulation_class.hpp"
 
+/* 
+ * Manipulation constructor, init planning_group 
+ */
 Manipulation::Manipulation(ros::NodeHandle nodeHandle, std::string planning_group)
 {
     PLANNING_GROUP = planning_group;
     grasp_config = nodeHandle.subscribe("/detect_grasps/clustered_grasps", 1, &Manipulation::callback, this);
     this->gripper_command = nodeHandle.advertise<control_msgs::GripperCommandActionGoal>("robotiq_2f_85_gripper_controller/gripper_cmd/goal", 10);
+    grasps_visualization_pub_ = nodeHandle.advertise<geometry_msgs::PoseArray>("grasps_visualization", 10);
 }
 
+/* 
+ * Get robot state and move to specified joint_group_position: 
+ */
 void Manipulation::getCurrentState()
 {
 
@@ -33,15 +44,9 @@ void Manipulation::setJointGroup(double j0, double j1, double j2, double j3, dou
     joint_group_positions[6] = j6; // gripper
 }
 
-/*
-void Manipulation::goTop()
-{
-    getCurrentState();
-    ROS_INFO("Moving to Top Position");
-    setJointGroup(0, pi / 4, 0, pi / 4, 0, pi / 1.75, -pi / 2);
-    move(joint_group_positions);
-}
-*/
+/* 
+ * Preset arm positions: 
+ */
 void Manipulation::goRight()
 {
     getCurrentState();
@@ -70,26 +75,18 @@ void Manipulation::goTop()
 {
     getCurrentState();
     ROS_INFO("Moving to Top Position");
-    setJointGroup(0, pi / 4, 0, pi / 4, 0, pi / 2, -pi / 2);
-    move(joint_group_positions);
-}
-/*
-void Manipulation::goRight()
-{
-    getCurrentState();
-    ROS_INFO("Moving to Right Position");
-    setJointGroup(pi / 2, pi / 2, -pi / 3, pi / 2, 0, pi / 4, pi / 7);
+    setJointGroup(0, pi / 6, 0, pi / 4, 0, pi / 1.75, -pi / 2);
     move(joint_group_positions);
 }
 
-void Manipulation::goLeft()
+void Manipulation::goPlace()
 {
     getCurrentState();
-    ROS_INFO("Moving to Left Position");
-    setJointGroup(-pi / 2, pi / 2, pi / 3, pi / 2, 0, pi / 4, 2.8);
+    ROS_INFO("Moving to Top Position");
+    setJointGroup(pi, pi / 4, 0, pi / 4, 0, pi / 2, -pi / 2);
     move(joint_group_positions);
 }
-*/
+
 void Manipulation::goWait()
 {
     getCurrentState();
@@ -98,13 +95,16 @@ void Manipulation::goWait()
     move(joint_group_positions);
 }
 
+/* 
+ * Set and remove objects for collision detection: 
+ */
 void Manipulation::set_objects()
 {
     std::vector<moveit_msgs::CollisionObject> collision_objects;
     collision_objects.resize(1);
 
     collision_objects[0].id = "object";
-    collision_objects[0].header.frame_id = "base_link"; //world
+    collision_objects[0].header.frame_id = "base_link"; //world for NERVE workstation
 
     // Define the primitive and its dimensions
     collision_objects[0].primitives.resize(1);
@@ -118,24 +118,60 @@ void Manipulation::set_objects()
     collision_objects[0].primitive_poses[0].position.x = this->pose.x;
     collision_objects[0].primitive_poses[0].position.y = this->pose.y;
     collision_objects[0].primitive_poses[0].position.z = this->pose.z;
+    //collision_objects[0].operation = collision_objects[0].ADD;
+
+    // Define the primitive and its dimensions.
+    collision_objects[0].id = "table";
+    collision_objects[0].header.frame_id = "base_link"; //world for NERVE workstation
+    collision_objects[0].primitives.resize(1);
+    collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
+    collision_objects[0].primitives[0].dimensions.resize(3);
+    collision_objects[0].primitives[0].dimensions[0] = 1;
+    collision_objects[0].primitives[0].dimensions[1] = 1;
+    collision_objects[0].primitives[0].dimensions[2] = 0;
+
+    // Define the pose of the table.
+    collision_objects[0].primitive_poses.resize(1);
+    collision_objects[0].primitive_poses[0].position.x = 0.4;
+    collision_objects[0].primitive_poses[0].position.y = 0;
+    collision_objects[0].primitive_poses[0].position.z = 0;
 
     collision_objects[0].operation = collision_objects[0].ADD;
+
     this->planning_scene_ptr->applyCollisionObjects(collision_objects);
-    this->collision_object = collision_objects[0];
 }
 
 void Manipulation::remove_objects()
 {
     std::vector<std::string> object_ids;
     object_ids.push_back("object");
+    object_ids.push_back("table");
     this->planning_scene_ptr->removeCollisionObjects(object_ids);
 }
 
+/* 
+ * Gripper Commands, TODO: replace with gripper planning group! 
+ */
+
+void Manipulation::close_gripper()
+{
+    this->gripper_cmd.goal.command.position = 0.5;
+    gripper_command.publish(gripper_cmd);
+    ROS_WARN("Closing gripper...");
+}
+
+void Manipulation::open_gripper()
+{
+    this->gripper_cmd.goal.command.position = 0;
+    gripper_command.publish(gripper_cmd);
+    ROS_WARN("Opening gripper...");
+}
+
+/* 
+ * Gripper Commands for pick() pipeline, not currently used 
+ */
 void Manipulation::closedGripper(trajectory_msgs::JointTrajectory &posture)
 {
-    //this->gripper_cmd.goal.command.position = 0.5;
-    //gripper_command.publish(gripper_cmd);
-    //ROS_WARN("CLOSE GRIPPER");
     //Add finger joints
     posture.joint_names.resize(6);
     posture.joint_names[0] = "left_inner_finger_joint";
@@ -144,7 +180,6 @@ void Manipulation::closedGripper(trajectory_msgs::JointTrajectory &posture)
     posture.joint_names[3] = "right_inner_finger_joint";
     posture.joint_names[4] = "left_inner_knuckle_joint";
     posture.joint_names[5] = "right_outer_knuckle_joint";
-
 
     // Set them as closed.
     posture.points.resize(1);
@@ -160,11 +195,7 @@ void Manipulation::closedGripper(trajectory_msgs::JointTrajectory &posture)
 
 void Manipulation::openGripper(trajectory_msgs::JointTrajectory &posture)
 {
-    //this->gripper_cmd.goal.command.position = 0;
-    //gripper_command.publish(gripper_cmd);
-    //ROS_WARN("OPEN GRIPPER");
 
-    //Add finger joints
     posture.joint_names.resize(6);
     posture.joint_names[0] = "left_inner_finger_joint";
     posture.joint_names[1] = "right_inner_knuckle_joint";
@@ -173,7 +204,6 @@ void Manipulation::openGripper(trajectory_msgs::JointTrajectory &posture)
     posture.joint_names[4] = "left_inner_knuckle_joint";
     posture.joint_names[5] = "right_outer_knuckle_joint";
 
-    // Set them as open, wide enough for the object to fit.
     posture.points.resize(1);
     posture.points[0].positions.resize(6);
     posture.points[0].positions[0] = 0;
