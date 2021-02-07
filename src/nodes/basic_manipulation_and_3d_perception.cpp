cef809d91ab8bc851954d838405539eec9e45721
kyassini <kevin_yassini@student.uml.edu>
2021-02-07 00:06:37 -0500

    Update main node to use new GPD pick function - Set objects before planning grasps - Add loop to run pick/place code until stopped

--- src/nodes/basic_manipulation_and_3d_perception.cpp
+++ src/nodes/basic_manipulation_and_3d_perception.cpp
@@ -1,9 +1,15 @@
+/*****************************************************************
+  Main Node for Gen3 Pick_and_Place
+  -Kevin Yassini (kevin_yassini@student.uml.edu)
+*****************************************************************/
+
 #include "manipulation_class.hpp"
 #include "perception_class.hpp"
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "basic_manipulation_and_3d_perception_node");
+
   ros::NodeHandle nh;
   ros::AsyncSpinner spinner(4);
   spinner.start();
@@ -22,24 +28,22 @@ int main(int argc, char **argv)
   manipulation.move_group_ptr = MoveGroupPtr(
       new moveit::planning_interface::MoveGroupInterface(manipulation.PLANNING_GROUP));
 
-  manipulation.move_group_ptr->detachObject("object");
-  manipulation.remove_objects();
-
-  manipulation.move_group_ptr->setPlanningTime(45.0);
-  manipulation.move_group_ptr->setMaxVelocityScalingFactor(0.50);
-  manipulation.move_group_ptr->setPlannerId("SBL");
-
-  // open gripper
-  manipulation.gripper_cmd.goal.command.position = 0;
-  manipulation.gripper_command.publish(manipulation.gripper_cmd);
-
-
-  //manipulation.open_gripper();
+    manipulation.move_group_ptr->setPlanningTime(45.0);
+    manipulation.move_group_ptr->setMaxVelocityScalingFactor(0.5);
+    manipulation.move_group_ptr->setPlannerId("SBL");
 
   while (ros::ok())
   {
+    // Detach objects potentially left over from previous run
+    manipulation.move_group_ptr->detachObject("object");
+    manipulation.remove_objects();
+
+    // Ensure gripper is open
+    manipulation.open_gripper();
+
+    // Take top snapshot, left and right angles also possible
     manipulation.goTop();
-    ros::Duration(3).sleep();
+    ros::Duration(1).sleep();
     perception.snapshot_top();
     ros::Duration(1).sleep();
     /*
@@ -53,26 +57,31 @@ int main(int argc, char **argv)
     perception.snapshot_left();
     ros::Duration(1).sleep();
     */
+
+    // Combine clouds and publish, for now only one cloud is used (top)
     perception.concatenate_clouds();
 
-    manipulation.goTop();
-    ros::Duration(5).sleep();
+    // Set collision objects (grasping object, table)
+    manipulation.set_objects();
 
+    // Get grasps grom GPD
     while (manipulation.getting_grasps)
     {
     }
 
+    // Main grasping code
     manipulation.path_planning();
     if (manipulation.grabbed_object)
     {
-      manipulation.set_objects();
-      manipulation.pick_and_place();
-
-      //manipulation.close_gripper();
+      manipulation.pickup();
+      manipulation.place(0.12); // Place at inputed height offset
     }
-    ros::Duration(5.0).sleep();
-    ros::shutdown();
+    ros::Duration(1.0).sleep();
+
+    // Go back to top position for repeating
+    manipulation.goTop();
   }
+
   ros::waitForShutdown();
 
   return 0;
